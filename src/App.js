cf5199ef79f49de4e9caca5c24909935f31bc410
Rubia <Rubiabufalofer@outlook.com>
2022-07-14 10:58:33 -0300

    poquemao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Destaques from './Destaques/Destaques';
 import Menu from './Menu/Menu';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
 
     <Menu />
+    <Destaques />
 
     </div>
    
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Menu.css'
 
 import logo from "../assets/images/logo.png"
 import {ReactComponent as Blocks} from "../assets/svg/blocks.svg"
 import {ReactComponent as List} from "../assets/svg/list.svg"
+import {ReactComponent as Hamburguer} from "../assets/svg/hamburger.svg";
+import {ReactComponent as Sun} from "../assets/svg/sun.svg";
 import Switch from '../Switch/Switch'
 
-
 const Menu = () => {
+    const [viewStyle, setViewStyle] = useState(0);
+
     return ( 
         <div className="Menu">
+            <div className="Menu-hamburguer">
+                <Hamburguer />
+            </div>
+
             <div className="Menu-left">
-                <Blocks className="selected"/>
-                <List />
+                <Blocks
+                    className={viewStyle === 0 ? 'selected' : ''}
+                    onClick={() => {setViewStyle(0)}}
+                />
+
+                <List 
+                    className={viewStyle === 1 ? 'selected' : ''}
+                    onClick={() => {setViewStyle(1)}}
+                />
             </div>
 
             <img src={logo} className="Menu-logo" />
             <div className="Menu-right">
+                <Sun />
                 <Switch />
             </div>
 
